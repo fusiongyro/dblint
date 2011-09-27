84a093f8f6bed6b7c4ccdf6143347f5714f0bedb
Daniel Lyons <dlyons@nrao.edu>
2011-09-27 15:49:36 -0600

    improving documentation

--- structure.pl
+++ structure.pl
@@ -1,7 +1,29 @@
 %% Database structure (Example)
 :- discontiguous(column/3, primary_key/2, unique/2, index/2, foreign_key/4).
 
+%% Structure of the sample database:
+%%
+%% CREATE TABLE managers (id SERIAL PRIMARY KEY);
+%%
+%% CREATE TABLE users (
+%%   id SERIAL PRIMARY KEY,
+%%   name VARCHAR UNIQUE,
+%%   manager_id INTEGER
+%% );
+%%
+%% CREATE TABLE projects (
+%%   id SERIAL PRIMARY KEY,
+%%   user_id INTEGER REFERENCES users,
+%%   tag1 VARCHAR,
+%%   tag2 VARCHAR,
+%%   tag3 VARCHAR,
+%%   access1 VARCHAR,
+%%   access2 VARCHAR
+%% );
+%%
+
 column(managers, id, integer).
+primary_key(managers, id).
 
 column(users, id, integer).
 column(users, name, varchar).
